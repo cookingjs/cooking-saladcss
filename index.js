5b0f7a040e2185980ebb525bd20f3b8a6e79d7b6
qingwei.li <cinwell.li@gmail.com>
2016-07-05 17:55:00 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * @param  {*} options - custom option
  */
 module.exports = function (cooking, options) {
+  var version = cooking.version
   var plugins = function (webpack) {
     options = options || {}
     options.features = options.features || {}
@@ -10,14 +11,17 @@ module.exports = function (cooking, options) {
     options.features.partialImport.addDependencyTo = webpack
 
     return [require('postcss-salad')(options)]
-  };
+  }
 
-  cooking.add('vue.autoprefixer', false)
-  cooking.add('preLoader.postcss', {
-    test: /\.css$/,
-    loaders: ['postcss-loader']
-  });
+  version = version ? Number(version.split('.')[0]) : 0
+  if (version < 1) {
+    cooking.add('vue.autoprefixer', false)
+    cooking.add('preLoader.postcss', {
+      test: /\.css$/,
+      loaders: ['postcss-loader']
+    })
+    cooking.add('vue.postcss', plugins)
+  }
 
-  cooking.add('postcss', plugins);
-  cooking.add('vue.postcss', plugins);
-};
+  cooking.add('postcss', plugins)
+}
