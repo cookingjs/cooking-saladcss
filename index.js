8c4c52ff3adb90852844aa882a00501438f75e79
qingwei.li <cinwell.li@gmail.com>
2016-07-26 16:19:43 +0800

    fix postcss bug

--- index.js
+++ index.js
@@ -3,19 +3,21 @@
  * @param  {*} options - custom option
  */
 module.exports = function (cooking, options) {
+  cooking.add('vue.autoprefixer', false)
+
   var version = cooking.version
+  version = version ? Number(version.split('.')[0]) : 0
+
   var plugins = function (webpack) {
     options = options || {}
     options.features = options.features || {}
     options.features.partialImport = options.features.partialImport || {}
     options.features.partialImport.addDependencyTo = webpack
 
-    return [require('postcss-salad')(options)]
+    return version < 1 ? [require('postcss-salad')(options)] : require('postcss-salad')(options)
   }
 
-  version = version ? Number(version.split('.')[0]) : 0
   if (version < 1) {
-    cooking.add('vue.autoprefixer', false)
     cooking.add('preLoader.postcss', {
       test: /\.css$/,
       loaders: ['postcss-loader']
