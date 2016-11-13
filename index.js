151b2eae4ab6714fc8aa21ae132030d07373f9b8
qingwei.li <cinwell.li@gmail.com>
2016-11-13 16:07:46 +0800

    support 1.2.0

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ module.exports = function (cooking, options) {
 
   var version = cooking.version
 
-  if (version >= '1.2.0') {
-    throw Error('不再支持该插件，请使用 postcss 配置文件进行配置，参考 https://github.com/michael-ciniawsky/postcss-load-config')
-  }
-
   version = version ? Number(version.split('.')[0]) : 0
 
   var plugins = function (webpack) {
